ba05437d4fb77a7dfd4cd1fdda80a07549e27a05
miraj <mirajulisam7993@gmail.com>
2020-11-21 22:32:13 +0600

    project create , configure model repository and properties

---
+++ Login_Registration_Server_Configuration/src/test/java/Login_Registration_Server_Configuration/LoginRegistrationServerConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package Login_Registration_Server_Configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegistrationServerConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
